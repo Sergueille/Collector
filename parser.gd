d2af6fbeb8abd8606c9ad00ceb05e9937e1d7f74
Cosmin Dinu--Thiery <cosmin.dinu-thiery@telecom-paris.fr>
2025-10-04 14:15:21 +0200

    parser

--- parser.gd
+++ parser.gd
@@ -4,21 +4,26 @@ func createMapFromFile(filename):
 	if not FileAccess.file_exists(filename):
 		return
 	var file = FileAccess.open(filename, FileAccess.READ)
-	var line
-	while(line = file.get_line() != EOF):
-		if line != "":
-			var name = line
-			var len = file.get_line()
-			var wid = file.get_line()
-			// Convertir dim en intj = file.get_8()
+	var line = file.get_line()
+	var name = line
+	var len = file.get_line()
+	var wid = file.get_line()
+	var coup_min = file.get_line()
+	#Convertir dim en intj = file.get_8()
+	
+	#var level = Level...
+	var property
+	for i in range(len):
+		for j in range(wid) : 
+			property = line[j] 
+			if porperty = "m" :
+				level.tiles[i][j] = Tile(i, j, property)
+			elif porperty = "v" :
+				level.tiles[i][j] = Tile(i, j, property)
+			elif porperty = "p" :
+				level.tiles[i][j] = Tile(i, j, property)
+			elif porperty = "d" : 
+				level.tiles[i][j] = Tile(i, j, property)
+			elif porperty = "a" :
+				level.tiles[i][j] = Tile(i, j, property)
 			
-			//var level = Level...
-			
-			for i in range(len):
-				var j = file.get_8()
-				var k = 0
-				while j != '\n':
-					var property = j... //Utilisation enum
-					level.tiles[i][k] = Tile(i, k, property)
-					k++
-					j = file.get_8()

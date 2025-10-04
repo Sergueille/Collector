e6ce998fea7ff40394dc14bc34536558ff766c5a
Sergueille Bismuth <>
2025-10-04 21:08:05 +0200

    Setup tileset

--- theme.gd
+++ theme.gd
@@ -1,14 +1,39 @@
 extends Resource
+class_name TileTheme
 
 @export var bg_color: Color
-@export var tiles_full: Array[Texture]
-@export var tiles_bl: Array[Texture]
-@export var tiles_br: Array[Texture]
-@export var tiles_tl: Array[Texture]
-@export var tiles_tr: Array[Texture]
+
+@export var tileset: TileSet
+@export var tiles_full: Array[Vector2i]
+@export var tiles_bl: Array[Vector2i]
+@export var tiles_br: Array[Vector2i]
+@export var tiles_tl: Array[Vector2i]
+@export var tiles_tr: Array[Vector2i]
+@export var tiles_b: Array[Vector2i]
+@export var tiles_t: Array[Vector2i]
+@export var tiles_l: Array[Vector2i]
+@export var tiles_r: Array[Vector2i]
+
+func get_tile_position(top: bool, right: bool, bottom: bool, left: bool) -> Vector2i:
+	if top and not right and not left and not bottom:
+		return get_random_from_arr(tiles_t)
+	elif bottom and not right and not left and not top:
+		return get_random_from_arr(tiles_b)
+	elif right and not top and not left and not bottom:
+		return get_random_from_arr(tiles_r)
+	elif left and not right and not top and not top:
+		return get_random_from_arr(tiles_l)
+	elif top and left and not right and not bottom:
+		return get_random_from_arr(tiles_tl)
+	elif top and right and not left and not bottom:
+		return get_random_from_arr(tiles_r)
+	elif bottom and left and not right and not top:
+		return get_random_from_arr(tiles_bl)
+	elif bottom and right and not left and not top:
+		return get_random_from_arr(tiles_br)
+	else:
+		return get_random_from_arr(tiles_full)
 
 
-@export var tiles_b: Array[Texture]
-@export var tiles_t: Array[Texture]
-@export var tiles_l: Array[Texture]
-@export var tiles_r: Array[Texture]
+func get_random_from_arr(arr: Array[Vector2i]) -> Vector2:
+	return arr[randi() % len(arr)]

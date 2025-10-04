cc2a5c8af9f7a71cbdf22966adb153f34113346b
Sergueille Bismuth <>
2025-10-04 13:15:10 +0200

    Many things

---
+++ theme.gd
@@ -0,0 +1,14 @@
+extends Resource
+
+@export var bg_color: Color
+@export var tiles_full: Array[Texture]
+@export var tiles_bl: Array[Texture]
+@export var tiles_br: Array[Texture]
+@export var tiles_tl: Array[Texture]
+@export var tiles_tr: Array[Texture]
+
+
+@export var tiles_b: Array[Texture]
+@export var tiles_t: Array[Texture]
+@export var tiles_l: Array[Texture]
+@export var tiles_r: Array[Texture]

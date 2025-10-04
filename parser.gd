c0bcf5db6f6eec4fae2b51c263c1dcbe9746ff03
Djildé-Mbénard Dabo <djilde-mbenard.dabo@etu.u-bordeaux.fr>
2025-10-04 11:09:28 +0200

    Parser prototype

--- parser.gd
+++ parser.gd
@@ -1,1 +1,24 @@
 extends Object
+
+func createMapFromFile(filename):
+	if not FileAccess.file_exists(filename):
+		return
+	var file = FileAccess.open(filename, FileAccess.READ)
+	var line
+	while(line = file.get_line() != EOF):
+		if line != "":
+			var name = line
+			var len = file.get_line()
+			var wid = file.get_line()
+			// Convertir dim en intj = file.get_8()
+			
+			//var level = Level...
+			
+			for i in range(len):
+				var j = file.get_8()
+				var k = 0
+				while j != '\n':
+					var property = j... //Utilisation enum
+					level.tiles[i][k] = Tile(i, k, property)
+					k++
+					j = file.get_8()

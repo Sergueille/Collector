c0411dee60d8d8f7171f9590da541c03e847795a
maximilien_dubus <maximilien.dubus@telecom-paris.fr>
2025-10-04 16:14:54 +0200

    modification UI

---
+++ UI/control.gd
@@ -0,0 +1,14 @@
+extends Control
+
+# Charger le script parser
+# const Parser = preload("res://parser.gd")
+
+
+func _on_play_button_1_pressed() -> void:
+#	var parser = Parser.new()
+#	parser.createMapFromFile("niveau1.txt")
+	get_tree().change_scene_to_file("res://UI/ingame_ui.tscn")
+
+
+func _on_play_button_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://UI/ingame_ui.tscn")
---
+++ UI/in_game_ui_control_menu.gd
@@ -0,0 +1,5 @@
+extends Control
+
+
+func _on_go_back_menu_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://UI/menu_ui.tscn")

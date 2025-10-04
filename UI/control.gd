f3cdc115d2e17bd6aa4ef430aff94bbbe1d38268
Sergueille Bismuth <>
2025-10-04 22:43:45 +0200

    Changed menus

--- UI/control.gd
+++ UI/control.gd
@@ -9,6 +9,9 @@ func _on_play_button_1_pressed() -> void:
 #	parser.createMapFromFile("niveau1.txt")
 	get_tree().change_scene_to_file("res://UI/ingame_ui.tscn")
 
+func _go_to_levels_screen() -> void:
+	get_tree().change_scene_to_file("res://UI/level_selection.tscn")
+
 
 func _on_play_button_2_pressed() -> void:
 	get_tree().change_scene_to_file("res://UI/ingame_ui.tscn")
@@ -16,3 +19,10 @@ func _on_play_button_2_pressed() -> void:
 
 func _on_credit_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://UI/credit/credit_ui.tscn")
+
+func _go_to_main_menu() -> void:
+	get_tree().change_scene_to_file("res://UI/menu_ui.tscn")
+
+
+func _on_levels_pressed() -> void:
+	pass # Replace with function body.

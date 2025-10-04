7ea67fe0a0141fedc0827bdb030d1caa92d60c08
Francisco Henriques <franciscolealhenriques@usp.br>
2025-10-04 10:52:54 +0200

    Add signal bus
    The signal bus makes so that we don't need to make connections by hand,
    and it makes sending signals through scenes easier.
    
    In a scene where you want to send signals, add a node with the
    signal_bus_sender.gd script.
    
    In a scene where you want to send signals, add a node with the
    signal_bus_receiver.gd script.
    
    Utilize the SignalBus class to send signals through scenes easily.
    
    Ask Chico for more information if needed :thumbs_up:

---
+++ util/signal_bus/signal_bus_receiver.gd
@@ -0,0 +1,29 @@
+class_name SignalBusReceiver
+extends Node
+
+# signal foo()
+# signal aaa(bbb: int)
+
+const GROUP := &'RECEIVER_GROUP'
+
+static var _s_dirty := true
+static var _s_receivers: Array[SignalBusReceiver] = []
+
+
+func _enter_tree():
+	add_to_group(GROUP)
+	_s_dirty = true
+
+
+func _exit_tree():
+	_s_dirty = true
+
+
+static func all(tree: SceneTree) -> Array[SignalBusReceiver]:
+	if _s_dirty:
+		_s_receivers.clear()
+		for receiver: SignalBusReceiver in tree.get_nodes_in_group(GROUP):
+			if receiver:
+				_s_receivers.append(receiver)
+		_s_dirty = false
+	return _s_receivers
---
+++ util/signal_bus/signal_bus_sender.gd
@@ -0,0 +1,9 @@
+class_name SignalBusSender
+extends Node
+
+
+# func send_foo():
+#    SignalBus.send(&"foo", [null])
+
+# func send_aaa(bbb: int):
+#	SignalBus.send(&"aaa", [bbb])

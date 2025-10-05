cdc4f10d50d1c2932f0797daa54c02adfa31c0b5
maximilien_dubus <maximilien.dubus@telecom-paris.fr>
2025-10-05 12:43:44 +0200

    ajout bouton

--- UI/in_game_ui_control_menu.gd
+++ UI/in_game_ui_control_menu.gd
@@ -4,3 +4,8 @@ extends Control
 func _on_go_back_menu_button_pressed() -> void:
 	SceneSwitcher.go_to_level(ResourceLoader.load("res://UI/menu_ui.tscn").instantiate())
 	get_tree().change_scene_to_file("res://UI/level_selection.tscn")
+
+
+func _on_restart_button_pressed() -> void:
+	var level = LevelParser.createMapFromFile("levels/niveau1.txt")
+	SceneSwitcher.go_to_level(level)

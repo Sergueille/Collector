500dafe14f2d2e681ab6a175a6895f027e1b581f
maximilien_dubus <maximilien.dubus@telecom-paris.fr>
2025-10-05 13:12:06 +0200

    Boutons selection des level triés

--- UI/level_selection.gd
+++ UI/level_selection.gd
@@ -4,25 +4,35 @@ extends Control
 var play_button_scene := preload("res://UI/play_button_N.tscn")
 
 func _ready() -> void:
-	SceneSwitcher.current_scene_root = self
 	var levels_path := "res://levels"
 	var dir := DirAccess.open(levels_path)
+	var files: Array[String] = []
+
 	if dir:
 		dir.list_dir_begin()
 		var file_name = dir.get_next()
 		while file_name != "":
 			if not dir.current_is_dir() and file_name.ends_with(".txt"):
-				_create_level_button(file_name)
+				files.append(file_name)
 			file_name = dir.get_next()
 		dir.list_dir_end()
 
+	# Tri par numéro
+	files.sort_custom(func(a, b):
+		var num_a = int(a.get_basename().replace("niveau", ""))
+		var num_b = int(b.get_basename().replace("niveau", ""))
+		return num_a < num_b
+	)
+
+	# Création des boutons dans l'ordre
+	for file_name in files:
+		_create_level_button(file_name)
+		
+
 func _create_level_button(file_name: String) -> void:
 	var play_button = play_button_scene.instantiate()
-
-
 	play_button.text = (file_name.get_basename()).replace("niveau", "")
 	$GridContainer.add_child(play_button)
-
 	# On capture le nom du fichier dans une lambda
 	play_button.pressed.connect(func():
 		_on_level_button_pressed(file_name))
